2018a218a1f1f97c18dfbc3fedb9ddf502bbb6dc
miigao <gaomi93@outlook.com>
2018-12-10 00:14:15 +0800

    1210

--- test.c
+++ test.c
@@ -4,35 +4,6 @@
 MPI_Status status;
 int rank, size, recvdata[32];
 
-int MPI_Gather_test(SendAddress, SendCount, SendDatatype,
-						RecvAddress, RecvCount, RecvDatatype, Root, comm)
-void *SendAddress;
-int SendCount;
-MPI_Datatype SendDatatype;
-void *RecvAddress;
-int RecvCount;
-MPI_Datatype RecvDatatype;
-int Root;
-MPI_Comm comm;
-{
-	int i, tmp = 47;
-	int *p;
-        if (rank != Root) {
-        	MPI_Send(&SendAddress, 1, MPI_INT, Root, 6, MPI_COMM_WORLD);
-                printf("senddata= %d\n", *(int *)SendAddress);
-        }
-	else {
-		for (i = 0; i < size; i++) {
-                    if (i == Root) continue;
-			MPI_Recv(&tmp, 1, MPI_INT, i, 6, MPI_COMM_WORLD, &status);
-			printf("good, tmp= %d\n", (int)tmp);
-			p = (int *)RecvAddress;
-			p[i] = tmp;
-		}
-	}
-	return 0;
-}
-
 int MPI_Allgather_test(SendAddress, SendCount, SendDatatype,
 						RecvAddress, RecvCount, RecvDatatype, comm)
 void *SendAddress;
@@ -43,10 +14,21 @@ int RecvCount;
 MPI_Datatype RecvDatatype;
 MPI_Comm comm;
 {
-	int i;
+	int i, j, tmp = 47, *p;
 	for (i = 0; i < size; i++) {
-		MPI_Gather_test(SendAddress, SendCount, SendDatatype,
-							RecvAddress, RecvCount, RecvDatatype, i, comm);
+		if (rank != i) {
+        	MPI_Send(&SendAddress, 1, MPI_INT, i, 6, MPI_COMM_WORLD);
+            printf("senddata= %d\n", *(int *)SendAddress);
+        }
+		else {
+			for (j = 0; j < size; j++) {
+                if (j == i) continue;
+				MPI_Recv(&tmp, 1, MPI_INT, j, 6, MPI_COMM_WORLD, &status);
+				printf("good, tmp= %d\n", (int)tmp);
+				p = (int *)RecvAddress;
+				p[i] = tmp;
+			}
+		}
 	}
 	return 0;
 }
@@ -60,13 +42,7 @@ char * *argv;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	senddata = rank + 77;
-	//MPI_Allgather_test(&senddata, 1, MPI_INT,
-	//			&recvdata, 1, MPI_INT, MPI_COMM_WORLD);
-        if (rank == 0)
-        MPI_Send(&senddata, 1, MPI_INT, 1, 6, MPI_COMM_WORLD);
-        else{
-        MPI_Recv(&tmp, 1, MPI_INT, i, 6, MPI_COMM_WORLD, &status);
-        recvdata[rank] = tmp;}
+	MPI_Allgather_test(&senddata, 1, MPI_INT, &recvdata, 1, MPI_INT, MPI_COMM_WORLD);
 	for (i = 0; i < size; i++) {
 		printf("rank= %d, recvdata[%d]= %d\n", rank, i, recvdata[i]);
 	}
