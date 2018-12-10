4214d745d61606bf4d2c790b06720060c6564a28
miigao <mig@mail.ustc.edu.cn>
2018-12-10 11:16:21 +0800

    12101116

--- test.c
+++ test.c
@@ -2,7 +2,11 @@
 #include<stdio.h>
 
 MPI_Status status;
-int rank, size, recvdata[32];
+int rank, size, recvdata[32], recvdata2[32];
+double starttime;
+double endtime;
+double time1;
+double time2;
 
 
 int main(argc, argv)
@@ -14,6 +18,10 @@ char * *argv;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	senddata = rank;
+        if (rank == 0) {
+            starttime = MPI_Wtime();
+        }
+        //My Allgather start.
         for (i = 0; i < size; i++) {
             if (rank != i) {
                     MPI_Send(&senddata, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
@@ -24,6 +32,19 @@ char * *argv;
                     MPI_Recv(&(recvdata[j]), 1, MPI_INT, j, tag, MPI_COMM_WORLD, &status);
                 }
             }
+        }
+        //My Allgather end.
+        if (rank == 0) {
+            endtime = MPI_Wtime();
+            printf("\nMy Allgather running time    = %f seconds\n",endtime-starttime);
+        }
+        if (rank == 0) {
+            time1 = MPI_Wtime();
+        }
+        MPI_Allgather(&senddata, 1, MPI_INT, recvdata2, 1, MPI_INT, MPI_COMM_WORLD);
+        if (rank == 0) {
+            time2 = MPI_Wtime();
+            printf("\nMPI_Allgather running time    = %f seconds\n",time2-time1);
         }
 	for (i = 0; i < size; i++) {
 		printf("rank= %d, recvdata[%d]= %d\n", rank, i, recvdata[i]);
